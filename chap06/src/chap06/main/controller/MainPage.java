f79b4e655a19a7d14099bf2849bcb25abf10951c
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-07-20 17:12:52 +0900

    메인 페이지 항목 추가

--- chap06/src/chap06/main/controller/MainPage.java
+++ chap06/src/chap06/main/controller/MainPage.java
@@ -1,11 +1,16 @@
 package chap06.main.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import chap06.dao.DBConnector;
+import chap06.dao.WorldcupDAO;
 
 public class MainPage extends HttpServlet {
 
@@ -13,8 +18,16 @@ public class MainPage extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
 		
-		// SELECT count(*) FROM worldcup; ÇÒ ¿¹Á¤
-		req.setAttribute("q_count", 0);
+		try (
+				Connection conn = DBConnector.getConnection();
+		) {
+			WorldcupDAO dao = new WorldcupDAO(conn);
+			req.setAttribute("q_count", dao.getCount());
+			req.setAttribute("q_count2", dao.getNotYetCount());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		
 		req.getRequestDispatcher("/WEB-INF/index/index.jsp").forward(req, resp);
 	}
